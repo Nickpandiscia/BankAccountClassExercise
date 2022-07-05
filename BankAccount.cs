f64f2a50fc12292a5dc71ab0f9aa8e24ccdae017
Nickpandiscia <npandiscia1@gmail.com>
2022-07-05 17:33:14 -0400

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double depositAmount)
+        {
+            Console.WriteLine($"Thank you for making a deposit of {depositAmount, 0:c}.");
+            balance = depositAmount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var theAccount = new BankAccount();
+            Console.WriteLine("Welcome to our bank. How much would you like to deposit today?");
+            double personsDeposit = double.Parse(Console.ReadLine());
+
+            theAccount.Deposit(personsDeposit);         
+            double personsBalance = theAccount.GetBalance();
+            Console.WriteLine($"Your current balance is {personsBalance, 0:c}.");
+            Console.WriteLine("Thank you for using our bank today!");
+            Console.ReadLine();
+            
+           
+        }
+    }
+}
